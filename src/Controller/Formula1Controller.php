921feb5e534a5422dfe72cf3d675ac53b964bf09
saurov-paul <saurov.paul1@gmail.com>
2024-04-25 14:28:24 +0300

    2nd commit

---
+++ src/Controller/Formula1Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Service\CallApiService;
+
+class Formula1Controller extends AbstractController
+
+{ 
+     #[Route('/', name: 'app_formula1')]
+     public function index(CallApiService $callApiService): Response
+         {
+            // dump($callApiService->getRacesIn2024());
+            return$this->render('formula1/index.html.twig',
+             ['data' => $callApiService->getRacesIn2024(),
+     ]);    
+    
+    }
+}
+
